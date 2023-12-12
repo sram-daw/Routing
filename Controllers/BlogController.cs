e83dc2f374161b525567122a7ffca52faf9a35e0
sram-daw <sara.ramilo.alonso@ciclosmontecastelo.com>
2023-12-12 18:53:08 +0100

    Agregar archivos de proyecto.

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Routing.Controllers
+{
+    public class BlogController : Controller
+    {
+        [Route("Tienda/index/{id:int}")]
+        public IActionResult Index(int id)
+        {
+			return Content("El ID es "+ id);
+        }
+
+		[Route("Producto/Detalles/{id:int:length(4)}-{slug:alpha}")]
+		public IActionResult Detalles(int id, string slug)
+		{
+			return Content("El ID es " + id+ " y el slug es"+slug);
+		}
+		[Route("Listado/{categoria?}/{marca?}")]
+		public IActionResult Listado(string categoria, string marca)
+		{
+			//return Content("Listamos los productos de "+ categoria+" y "+marca);
+			return Content($"Listamos los productos de {categoria} y {marca}");
+		}
+		[Route("Blog/Enumerar")] //otra forma de asignar rutas sin necesidad de usar Program.cs
+        public IActionResult Listar()
+        {
+            return Content("Listamos los posts del blog");
+        }
+        [Route("Blog/Detalle/{id:int}")] //solo entrará en esta acción si el id es un numero
+        public IActionResult Detalle(int idPublicacion)
+        {
+            return Content("Muestra detalles de un post");
+        }
+        public IActionResult Crear(string nombre)
+        {
+            return Content("Creamos nueva publicacion.");
+        }
+        public IActionResult Editar(string nombre)
+        {
+            return Content("Modificamos nueva publicacion.");
+        }
+        public IActionResult Eliminar(string nombre)
+        {
+            return Content("Eliminamos una publicacion.");
+        }
+     
+    }
+}
---
+++ Controllers/ProductoController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Routing.Controllers
+{
+	public class ProductoController : Controller
+	{
+		public IActionResult Index()
+		{
+			return Content("Esto es el index");
+		}
+		public IActionResult Listar()
+		{
+			return Content("Listamos los productos");
+		}
+		public IActionResult Detalle(int idProducto)
+		{
+			return Content("Detallamos un producto específico");
+		}
+		//actividad 3
+		[Route("Producto/Buscar/{terminoBusqueda?}")]
+		public IActionResult Buscar(string terminoBusqueda)
+		{
+			if (terminoBusqueda == null || terminoBusqueda == "")
+			{
+				return RedirectToAction("Index");
+			}
+			return Content($"Buscamos el término {terminoBusqueda}");
+		}
+
+		[Route("Producto/Agregar/{producto?}")]
+		public IActionResult Agregar(Producto producto)
+		{
+
+			if (producto == null) {
+				return Content("Formulario agregar");
+			}
+			//llamamos al modelo para agregarlo a la bd
+			return RedirectToAction("Index");
+		}
+
+		[Route("Producto/Editar/{id?}/{producto?}")]
+		public IActionResult Editar(int id, Producto producto)
+		{
+
+			if (id == 0) //aparentemente si no se rellena un parametro de tipo int es igual a 0
+			{
+				return Content("Index1");
+			}
+			if (producto == null)
+			{
+				return Content("Formulario Agregar");
+			}
+			else {
+				//actualizar producto
+				return Content("Indexxx");
+			}
+			
+			
+		}
+	}
+	public class Producto //creamos aquí mismo la clase Producto para el ejemplo de Agregar
+	{
+
+	}
+
+}
+
+
